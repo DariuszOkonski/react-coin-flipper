c2c6d8bb40c4ed19d5bfd6816f205b869f962595
Dariusz Okonski <darek200180@gmail.com>
2019-12-02 22:33:06 +0000

    Updates

---
+++ static/js/main.00fdd433.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp017-coin-flipper"]=this["webpackJsonp017-coin-flipper"]||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var r=t(0),c=t.n(r),i=t(2),p=t.n(i);t(8);var a=function(){return c.a.createElement("div",{className:"App"},c.a.createElement("h1",null,"Coin Flipper"))};p.a.render(c.a.createElement(a,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.00fdd433.chunk.js.map
