c2bd12adede41a072d1e4b4a201a9570d5ccca9f
Dariusz Okonski <darek200180@gmail.com>
2019-12-03 20:52:32 +0000

    display Home component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
+import Home from './Home';
 
 function App() {
   return (
@@ -8,9 +9,9 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navigation />
 
-        <div className="container">
+        <div className="container appContainer">
           <Switch>
-            <Route exact path='/' render={() => <h2>Home</h2>} />
+            <Route exact path='/' component={Home} />
             <Route exact path='/game' render={() => <h2>Game</h2>} />
             <Route exact path='/about' render={() => <h2>About</h2>} />
             <Route render={() => <h2>Error</h2>} />
