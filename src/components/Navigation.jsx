0a5813b017c160da36ccefa7f509ddaa2da00afa
Dariusz Okonski <darek200180@gmail.com>
2019-12-03 20:24:08 +0000

    add Navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">Coin Flipper</NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" exact to="/">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" exact to="/game">Game</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" exact to="/about">About</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
