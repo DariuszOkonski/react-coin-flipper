443e0c2324579652843cdb6a57fae91bff636eac
Dariusz Okonski <darek200180@gmail.com>
2019-12-03 19:59:06 +0000

    wrap BrowserRouter inside App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Coin Flipper</h1>
-    </div>
+    <>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <h1>Coin Flipper</h1>
+      </BrowserRouter>
+    </>
   );
 }
 
