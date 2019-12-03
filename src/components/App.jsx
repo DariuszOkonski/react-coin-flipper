1f1d466602801e7d266fdad77d154f3ed767ca97
Dariusz Okonski <darek200180@gmail.com>
2019-12-03 20:29:43 +0000

    add routing to all paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
 
 function App() {
@@ -7,6 +7,15 @@ function App() {
     <div>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navigation />
+
+        <div className="container">
+          <Switch>
+            <Route exact path='/' render={() => <h2>Home</h2>} />
+            <Route exact path='/game' render={() => <h2>Game</h2>} />
+            <Route exact path='/about' render={() => <h2>About</h2>} />
+            <Route render={() => <h2>Error</h2>} />
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   );
