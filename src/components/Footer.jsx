66fdb3a33e0c8a0b0c33cae5c6206891f73d2d96
Dariusz Okonski <darek200180@gmail.com>
2019-12-04 01:31:35 +0000

    add component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="sticky-footer" className="py-1 bg-primary text-light fixed-bottom">
+      <div className="container text-center">
+        Copyright &copy; 2019 Dariusz Okonski
+          </div>
+    </footer>
+  );
+}
+
+export default Footer;
