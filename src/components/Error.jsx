7354909378258bc9e9d4c2a1295325a3dbb6119b
Dariusz Okonski <darek200180@gmail.com>
2019-12-04 01:20:18 +0000

    add Error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className="jumbotron bg-danger">
+      <h1 className="display-4 text-white text-center">Error - no such a page</h1>
+    </div>
+  );
+}
+
+export default Error;
