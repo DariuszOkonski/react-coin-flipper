9f5f8ad5b74c75baf887ee271b4e4cd95f7bd4af
Dariusz Okonski <darek200180@gmail.com>
2019-12-04 00:14:55 +0000

    change brandname to DazzDev

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
     <div>
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
         <div className="container">
-          <NavLink className="navbar-brand" to="/">CF</NavLink>
+          <NavLink className="navbar-brand" to="/">DazzDev</NavLink>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
